41badcdf4038f8d27eb9002e9cf6d015eaf93383
brianparkerin <brianparkerin@gmail.com>
2020-03-19 20:43:17 +0100

    first commit

---
+++ entorno/entorno/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\brian\Desktop\Practica-Django\entorno\entorno\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
